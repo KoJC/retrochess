53860bf56a9cd80e5136ee72d4c1716ea97e7990
KoJC <konrad.creutzburg@outlook.de>
2019-05-23 20:20:40 +0200

    main add

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int i;
+
+int main() 
+{
+    cout << "Hello, World!\n\n";
+    cout << "Hello, World2!\n\n";
+    i=0;
+    while (i<10){
+        i++;
+         cout << i;
+    }
+
+    return 0;
+}
